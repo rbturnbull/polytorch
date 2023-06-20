21a4d89e9732e4591654f4c3e606b065641e5437
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-06-20 13:20:55 +1000

    :zap: importing more functions and classes into __init__.py

--- polytorch/__init__.py
+++ polytorch/__init__.py
@@ -1,2 +1,6 @@
+from .data import CategoricalData, ContinuousData, OrdinalData, PolyData
+from .embedding import PolyEmbedding
 from .loss import PolyLoss
-from .data import CategoricalData, ContinuousData, OrdinalData
+from .enums import ContinuousDataLossType
+from .modules import PolyLinear
+from .util import split_tensor, total_size
