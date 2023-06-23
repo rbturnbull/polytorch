193b7f2c7ec8e02adca1a95ffe4464b2299ea8a0
Robert Turnbull <robert.turnbull@unimelb.edu.au>
2023-06-23 10:38:31 +1000

    :zap: adding option to assign colors and labels to embedding plots

--- polytorch/data.py
+++ polytorch/data.py
@@ -1,19 +1,27 @@
 from torch import nn
+from typing import List
 from dataclasses import dataclass
 
 from .enums import ContinuousDataLossType
 
 class PolyData():
+    name:str = ""
+
     def create_module(self, embedding_size:int):
         raise NotImplementedError(f'Use either CategoricalData, OrdinalData or ContinuousData')
 
     def size(self) -> int:
         raise NotImplementedError(f'Use either CategoricalData, OrdinalData or ContinuousData')
-    
+
+    def get_name(self) -> str:
+        return self.name or self.__class__.__name__
+
 
 @dataclass
 class CategoricalData(PolyData):
     category_count:int
+    labels:List[str] = None
+    colors:List[str] = None
 
     def create_module(self, embedding_size:int):
         return nn.Embedding(self.category_count, embedding_size)
@@ -24,6 +32,7 @@ class CategoricalData(PolyData):
 
 @dataclass
 class OrdinalData(CategoricalData):
+    color:str = ""
     # add in option to estimate distances or to set them?
 
     def create_module(self, embedding_size:int):
@@ -34,6 +43,7 @@ class OrdinalData(CategoricalData):
 @dataclass
 class ContinuousData(PolyData):
     loss_type:ContinuousDataLossType = ContinuousDataLossType.SMOOTH_L1_LOSS
+    color:str = ""
 
     def create_module(self, embedding_size:int):
         from .embedding import ContinuousEmbedding
--- polytorch/plots.py
+++ polytorch/plots.py
@@ -4,18 +4,19 @@ from pathlib import Path
 from .embedding import PolyEmbedding
 import plotly.graph_objects as go
 from sklearn.decomposition import PCA
+from itertools import cycle
 
 import torch
 
 from .embedding import ContinuousEmbedding, OrdinalEmbedding
-from .data import CategoricalData
+from .data import CategoricalData, OrdinalData
 
 
 def format_fig(fig):
     """Formats a plotly figure in a nicer way."""
     fig.update_layout(
-        width=1200,
-        height=550,
+        width=1000,
+        height=800,
         plot_bgcolor="white",
         title_font_color="black",
         font=dict(
@@ -28,8 +29,8 @@ def format_fig(fig):
     fig.update_xaxes(gridcolor=gridcolor)
     fig.update_yaxes(gridcolor=gridcolor)
 
-    fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True, ticks='outside')
-    fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True, ticks='outside')
+    fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True, ticks='outside', zeroline=True, zerolinewidth=1, zerolinecolor='black')
+    fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True, ticks='outside', zeroline=True, zerolinewidth=1, zerolinecolor='black')
 
     return fig
 
@@ -37,18 +38,35 @@ def format_fig(fig):
 def plot_embedding(embedding:PolyEmbedding, n_components:int=2, show:bool=False, output_path:str|Path|None=None) -> go.Figure:
     # get embedding weights
     weights = []
+    labels = []
+    colors = []
+
+    # same as px.colors.qualitative.Plotly
+    cmap = cycle(['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52'])
+
     for input_type, module in zip(embedding.input_types, embedding.embedding_modules):
         weight = module.weight
         if len(weight.shape) == 1:
             weight = weight.unsqueeze(0)
         
         weights.append(weight)
-        # if isinstance(module, (OrdinalEmbedding, ContinuousEmbedding)):
-        #     weights.append(module.weight.detach())
-        # elif isinstance(input_type, CategoricalData):
-        #     weights.append(module.weight.detach())
-        # else:
-        #     raise ValueError("Unknown input type")
+        
+        if isinstance(input_type, CategoricalData) and not isinstance(input_type, OrdinalData):
+            my_labels = (
+                input_type.labels if input_type.labels is not None 
+                else [f"{input_type.get_name()}_{i}" for i in range(input_type.category_count)]
+            )
+            labels.extend(my_labels)
+
+            my_colors = (
+                input_type.colors if input_type.colors is not None 
+                else [next(cmap) for _ in range(input_type.category_count)]
+            )
+            colors.extend(my_colors)
+        else:
+            labels.append(input_type.get_name())
+            colors.append(input_type.color if input_type.color else next(cmap))
+
     weights = torch.cat(weights, dim=0).detach()
 
     # PCA
@@ -58,28 +76,35 @@ def plot_embedding(embedding:PolyEmbedding, n_components:int=2, show:bool=False,
     # plot
     fig = go.Figure()
 
-    if n_components == 2:
-        fig.add_trace(go.Scatter(
-            x=weights_reduced[:,0], 
-            y=weights_reduced[:,1],
-            mode='markers',
-        ))
-        fig.update_xaxes(title_text="Component 1")
-        fig.update_yaxes(title_text="Component 2")
-    elif n_components == 3:
-        fig.add_trace(go.Scatter3d(
-            x=weights_reduced[:,0], 
-            y=weights_reduced[:,1],
-            z=weights_reduced[:,2],
-            mode='markers',
-        ))
-        fig.update_layout(scene = dict(
-            xaxis_title='Component 1',
-            yaxis_title='Component 2',
-            zaxis_title='Component 3',
-        ))
-    else:
-        raise ValueError(f"n_components must be 2 or 3, not {n_components}")
+    for vector, label, color in zip(weights_reduced, labels, colors):
+        if n_components == 2:
+            fig.add_trace(go.Scatter(
+                x=[vector[0]],
+                y=[vector[1]],
+                mode='markers',
+                name=label,
+                marker_color=color,
+            ))
+            fig.update_xaxes(title_text="Component 1")
+            fig.update_yaxes(title_text="Component 2")
+        elif n_components == 3:
+            fig.add_trace(go.Scatter3d(
+                x=[vector[0]],
+                y=[vector[1]],
+                z=[vector[2]],
+                mode='markers',
+                name=label,
+                marker_color=color,
+            ))
+            fig.update_layout(scene = dict(
+                xaxis_title='Component 1',
+                yaxis_title='Component 2',
+                zaxis_title='Component 3',
+            ))
+        else:
+            raise ValueError(f"n_components must be 2 or 3, not {n_components}")
+        
+    fig.update_layout(showlegend=True)
     format_fig(fig)
 
     # output image as file
